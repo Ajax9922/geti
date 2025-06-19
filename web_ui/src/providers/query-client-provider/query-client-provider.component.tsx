01a3732cc6172a9c2d0e144cc7d6054626a46e61
Joao Vilaca <joao.vilaca@intel.com>
2025-06-19 10:54:17 +0200

    Refactor query-client-provider and required-providers render to use the actual client used in the app

--- web_ui/src/providers/query-client-provider/query-client-provider.component.tsx
+++ web_ui/src/providers/query-client-provider/query-client-provider.component.tsx
@@ -6,6 +6,7 @@ import { ReactNode, useLayoutEffect, useMemo, useRef } from 'react';
 import { getErrorMessage } from '@geti/core/src/services/utils';
 import {
     DefaultOptions,
+    MutationCache,
     QueryCache,
     QueryClient,
     QueryClientProvider as TanstackQueryClientProvider,
@@ -14,7 +15,63 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { isAxiosError } from 'axios';
 
 import { NOTIFICATION_TYPE } from '../../notification/notification-toast/notification-type.enum';
-import { useNotification } from '../../notification/notification.component';
+import { AddNotificationProps, useNotification } from '../../notification/notification.component';
+
+export const createGetiQueryClient = ({
+    defaultQueryOptions,
+    addNotification,
+}: {
+    defaultQueryOptions?: DefaultOptions;
+    addNotification: (props: AddNotificationProps) => string;
+}): QueryClient => {
+    const notify = { current: addNotification };
+
+    const queryCache = new QueryCache({
+        onError: (error, query) => {
+            if (isAxiosError(error) && query.meta && 'notifyOnError' in query.meta) {
+                const message = query.meta.errorMessage;
+                if (query.meta.notifyOnError === true) {
+                    notify.current({
+                        message: typeof message === 'string' ? message : getErrorMessage(error),
+                        type: NOTIFICATION_TYPE.ERROR,
+                    });
+                }
+            }
+
+            if (query.meta && 'disableGlobalErrorHandling' in query.meta) {
+                if (query.meta.disableGlobalErrorHandling === true) {
+                    return;
+                }
+            }
+        },
+    });
+
+    const mutationCache = new MutationCache({
+        onError: (error) => {
+            // TODO: If we decide to add an "opt-out" mechanism we can use the same logic
+            // as query cache and use the `meta` field
+            if (isAxiosError(error)) {
+                const message = error.message;
+                notify.current({
+                    message: typeof message === 'string' ? message : getErrorMessage(error),
+                    type: NOTIFICATION_TYPE.ERROR,
+                });
+            }
+        },
+    });
+
+    return new QueryClient({
+        defaultOptions: {
+            queries: {
+                refetchIntervalInBackground: false,
+                refetchOnWindowFocus: false,
+            },
+            ...defaultQueryOptions,
+        },
+        queryCache,
+        mutationCache,
+    });
+};
 
 declare module '@tanstack/react-query' {
     interface Register {
@@ -32,9 +89,11 @@ declare module '@tanstack/react-query' {
 export const QueryClientProvider = ({
     children,
     defaultQueryOptions,
+    customQueryClient,
 }: {
     children: ReactNode;
     defaultQueryOptions?: DefaultOptions;
+    customQueryClient?: QueryClient;
 }) => {
     const { addNotification } = useNotification();
 
@@ -46,37 +105,12 @@ export const QueryClientProvider = ({
     }, [addNotification]);
 
     const queryClient = useMemo(() => {
-        const queryCache = new QueryCache({
-            onError: (error, query) => {
-                if (isAxiosError(error) && query.meta && 'notifyOnError' in query.meta) {
-                    const message = query.meta.errorMessage;
-                    if (query.meta.notifyOnError === true) {
-                        notify.current({
-                            message: typeof message === 'string' ? message : getErrorMessage(error),
-                            type: NOTIFICATION_TYPE.ERROR,
-                        });
-                    }
-                }
+        if (customQueryClient) {
+            return customQueryClient;
+        }
 
-                if (query.meta && 'disableGlobalErrorHandling' in query.meta) {
-                    if (query.meta.disableGlobalErrorHandling === true) {
-                        return;
-                    }
-                }
-            },
-        });
-
-        return new QueryClient({
-            defaultOptions: {
-                queries: {
-                    refetchIntervalInBackground: false,
-                    refetchOnWindowFocus: false,
-                },
-                ...defaultQueryOptions,
-            },
-            queryCache,
-        });
-    }, [defaultQueryOptions]);
+        return createGetiQueryClient({ addNotification, defaultQueryOptions });
+    }, [addNotification, customQueryClient, defaultQueryOptions]);
 
     return (
         <TanstackQueryClientProvider client={queryClient}>
