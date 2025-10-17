26889de3c5a33c5747ce2c089268373317c97f61
Katarzyna Romanowska <katarzyna.romanowska@intel.com>
2025-10-17 08:57:00 +0200

    remove doubled Workspace word from workspace role column

--- web_ui/src/pages/user-management/users/users-table/workspaces-role-cell.component.tsx
+++ web_ui/src/pages/user-management/users/users-table/workspaces-role-cell.component.tsx
@@ -3,7 +3,7 @@
 
 import { RESOURCE_TYPE, Role } from '@geti/core/src/users/users.interface';
 import { Workspace } from '@geti/core/src/workspaces/services/workspaces.interface';
-import { isEmpty } from 'lodash-es';
+import { capitalize, isEmpty } from 'lodash-es';
 
 import { CasualCell } from '../../../../shared/components/table/components/casual-cell/casual-cell.component';
 import { TableCellProps } from '../../../../shared/components/table/table.interface';
@@ -16,8 +16,7 @@ interface WorkspacesRoleCellProps extends Omit<TableCellProps, 'cellData'> {
 
 export const WorkspacesRoleCell = ({ cellData, workspaceId, workspaces, ...rest }: WorkspacesRoleCellProps) => {
     const workspaceRoles = cellData.filter((role) => role.resourceType === RESOURCE_TYPE.WORKSPACE);
-    const workspaceRole = workspaceRoles.find((role) => role.resourceId === workspaceId)?.role;
-    const selectedWorkspaceRole = workspaceRole ? `Workspace ${workspaceRole}` : '';
+    const selectedWorkspaceRole = capitalize(workspaceRoles.find((role) => role.resourceId === workspaceId)?.role);
     const availableWorkspaces = workspaceRoles
         .map((role) => workspaces.find((workspace) => workspace.id === role.resourceId)?.name ?? role.resourceId)
         .join(', ');
