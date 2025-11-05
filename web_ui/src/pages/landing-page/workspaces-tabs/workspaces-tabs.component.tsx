066eb67079786700230adaaf324217a5d04c88d0
Joao Vilaca <joao.vilaca@intel.com>
2025-11-05 11:22:57 +0100

    Use new component on workspaces-tabs

--- web_ui/src/pages/landing-page/workspaces-tabs/workspaces-tabs.component.tsx
+++ web_ui/src/pages/landing-page/workspaces-tabs/workspaces-tabs.component.tsx
@@ -7,129 +7,93 @@ import { isOrganizationAdmin } from '@geti/core/src/users/user-role-utils';
 import { RESOURCE_TYPE } from '@geti/core/src/users/users.interface';
 import { useWorkspacesApi } from '@geti/core/src/workspaces/hooks/use-workspaces.hook';
 import { WorkspaceEntity } from '@geti/core/src/workspaces/services/workspaces.interface';
-import { ActionButton, Flex, Item, Loading, TabList, TabPanels, Tabs, Tooltip, TooltipTrigger } from '@geti/ui';
-import { Add } from '@geti/ui/icons';
 import { useOverlayTriggerState } from 'react-stately';
 
 import { useOrganizationIdentifier } from '../../../hooks/use-organization-identifier/use-organization-identifier.hook';
 import { CustomTabItem } from '../../../shared/components/custom-tab-item/custom-tab-item.component';
 import { HasPermission } from '../../../shared/components/has-permission/has-permission.component';
 import { OPERATION } from '../../../shared/components/has-permission/has-permission.interface';
-import { TabItem } from '../../../shared/components/tabs/tabs.interface';
-import { hasEqualId } from '../../../shared/utils';
+import { ManagedTabs } from '../../../shared/components/managed-tabs/managed-tabs.component';
 import { CreateWorkspaceDialog } from '../../user-management/workspaces/create-workspace-dialog/create-workspace-dialog.component';
 import { LandingPageWorkspace as Workspace } from '../landing-page-workspace/landing-page-workspace.component';
 import { NoPermissionPlaceholder } from './components/no-permission-placeholder.component';
 import { CustomTabItemWithMenu } from './custom-tab-item-with-menu.component';
 import { useWorkspacesTabs } from './hooks/use-pinned-collapsed-workspace.hook';
 
-import classes from '../../../shared/components/custom-tab-item/custom-tab-item.module.scss';
+const WorkspaceItem = ({ workspace }: { workspace: WorkspaceEntity }) => {
+    const { organizationId } = useOrganizationIdentifier();
+    const { data: activeUser } = useActiveUser(organizationId);
+    const { workspaces, selectWorkspace, selectedWorkspaceId } = useWorkspacesTabs();
+    const { FEATURE_FLAG_WORKSPACE_ACTIONS } = useFeatureFlags();
+
+    const isSelected = workspace.id === selectedWorkspaceId;
+
+    if (isSelected && FEATURE_FLAG_WORKSPACE_ACTIONS) {
+        return (
+            <HasPermission
+                operations={[OPERATION.WORKSPACE_MANAGEMENT]}
+                resources={[{ type: RESOURCE_TYPE.WORKSPACE, id: workspace.id }]}
+                specialCondition={activeUser !== undefined && isOrganizationAdmin(activeUser, organizationId)}
+                Fallback={<CustomTabItem name={workspace.name} isMoreIconVisible={false} />}
+            >
+                <CustomTabItemWithMenu
+                    workspace={workspace}
+                    isMoreIconVisible={true}
+                    workspaces={workspaces}
+                    selectWorkspace={selectWorkspace}
+                />
+            </HasPermission>
+        );
+    }
+
+    return <CustomTabItem name={workspace.name} isMoreIconVisible={false} />;
+};
 
 export const WorkspacesTabs = () => {
     const { organizationId } = useOrganizationIdentifier();
-    const { workspaces, selectWorkspace, selectedWorkspaceId, handleSelectWorkspace } = useWorkspacesTabs();
+    const { workspaces, selectedWorkspaceId, handleSelectWorkspace } = useWorkspacesTabs();
     const { FEATURE_FLAG_WORKSPACE_ACTIONS } = useFeatureFlags();
-    const { data: activeUser } = useActiveUser(organizationId);
     const createWorkspaceDialogState = useOverlayTriggerState({});
 
     const { useCreateWorkspaceMutation } = useWorkspacesApi(organizationId);
     const createWorkspace = useCreateWorkspaceMutation();
-    const selectedWorkspace = workspaces.find(hasEqualId(selectedWorkspaceId));
-
-    const items = workspaces.map(({ id, name }) => ({
-        name,
-        id: `${id === selectedWorkspaceId ? 'selected-' : ''}workspace-${id}`,
-        key: id,
-        children: <Workspace />,
-    }));
     const workspacesNames = workspaces.map((workspace) => workspace.name);
 
     return (
-        <Flex id={`page-layout-id`} direction='column' height='100%' UNSAFE_className={classes.componentWrapper}>
-            <Tabs
+        <>
+            <ManagedTabs<WorkspaceEntity>
+                id='page-layout-id'
+                items={workspaces}
                 selectedKey={selectedWorkspaceId}
-                items={items}
-                aria-label={'Workspaces tabs'}
-                height={'100%'}
-                width={'100%'}
-                orientation={'vertical'}
                 onSelectionChange={handleSelectWorkspace}
-            >
-                <Flex width={'100%'} alignItems={'center'} UNSAFE_className={classes.tabWrapper} gap={'size-200'}>
-                    <div className={classes.tabListScrollContainer}>
-                        <TabList UNSAFE_className={classes.tabList}>
-                            {(item: TabItem) => (
-                                <Item textValue={item.name as string} key={item.key}>
-                                    <>
-                                        <Flex alignItems={'center'}>
-                                            {selectedWorkspaceId === item.key && FEATURE_FLAG_WORKSPACE_ACTIONS ? (
-                                                <HasPermission
-                                                    operations={[OPERATION.WORKSPACE_MANAGEMENT]}
-                                                    resources={[{ type: RESOURCE_TYPE.WORKSPACE, id: item.id }]}
-                                                    specialCondition={
-                                                        activeUser !== undefined &&
-                                                        isOrganizationAdmin(activeUser, organizationId)
-                                                    }
-                                                    Fallback={
-                                                        <CustomTabItem
-                                                            name={item.name as string}
-                                                            isMoreIconVisible={false}
-                                                        />
-                                                    }
-                                                >
-                                                    <CustomTabItemWithMenu
-                                                        workspace={selectedWorkspace as WorkspaceEntity}
-                                                        isMoreIconVisible={item.key === selectedWorkspaceId}
-                                                        workspaces={workspaces}
-                                                        selectWorkspace={selectWorkspace}
-                                                    />
-                                                </HasPermission>
-                                            ) : (
-                                                <CustomTabItem name={item.name as string} isMoreIconVisible={false} />
-                                            )}
-                                        </Flex>
-                                    </>
-                                </Item>
-                            )}
-                        </TabList>
-                    </div>
-
-                    {FEATURE_FLAG_WORKSPACE_ACTIONS && (
-                        <HasPermission operations={[OPERATION.WORKSPACE_CREATION]}>
-                            <TooltipTrigger placement={'bottom'}>
-                                <ActionButton
-                                    isQuiet
-                                    id={'create-new-workspace-id'}
-                                    aria-label={'Create new workspace'}
-                                    onPress={createWorkspaceDialogState.open}
-                                    isDisabled={createWorkspace.isPending}
-                                >
-                                    {createWorkspace.isPending ? <Loading mode='inline' size={'S'} /> : <Add />}
-                                </ActionButton>
-                                <Tooltip>Create a new workspace</Tooltip>
-                            </TooltipTrigger>
-                        </HasPermission>
-                    )}
-                </Flex>
-                <TabPanels>
-                    {(item: TabItem) => (
-                        <Item key={item.key}>
-                            <HasPermission
-                                operations={[OPERATION.CAN_SEE_WORKSPACE]}
-                                specialCondition={!FEATURE_FLAG_WORKSPACE_ACTIONS || undefined}
-                                Fallback={<NoPermissionPlaceholder />}
-                            >
-                                {item.children}
-                            </HasPermission>
-                        </Item>
-                    )}
-                </TabPanels>
-            </Tabs>
+                renderTabItem={(workspace) => <WorkspaceItem workspace={workspace} />}
+                renderTabPanel={() => (
+                    <HasPermission
+                        operations={[OPERATION.CAN_SEE_WORKSPACE]}
+                        specialCondition={!FEATURE_FLAG_WORKSPACE_ACTIONS || undefined}
+                        Fallback={<NoPermissionPlaceholder />}
+                    >
+                        <Workspace />
+                    </HasPermission>
+                )}
+                addButton={
+                    FEATURE_FLAG_WORKSPACE_ACTIONS
+                        ? {
+                              id: 'create-new-workspace-id',
+                              ariaLabel: 'Create new workspace',
+                              tooltipText: 'Create a new workspace',
+                              onPress: createWorkspaceDialogState.open,
+                              isLoading: createWorkspace.isPending,
+                          }
+                        : undefined
+                }
+                ariaLabel='Workspaces tabs'
+            />
             <CreateWorkspaceDialog
                 triggerState={createWorkspaceDialogState}
                 names={workspacesNames}
                 nameLimitations={{ maxLength: 64, minLength: 1 }}
             />
-        </Flex>
+        </>
     );
 };
